75efaaeb637dc2f405ae426c63117f47fe0a0469
Michele H <michele_h@outlook.com>
2022-03-07 15:04:27 -0800

    changes

--- Fundamentals/Fundamentals/hello_world.py
+++ Fundamentals/Fundamentals/hello_world.py
@@ -1,16 +1,25 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
+
+print("-" * 25)
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
-print("Hello Noelle", name)	# with a comma
-print("Hello Noelle" + name)	# with a +
+name = "Michele"
+print("Hello", name)	# with a comma
+print("Hello " + name)	# with a +
+
+print("-" * 25)
 # 3. print "Hello 42!" with the number in a variable
 name = 42
-print( your code here )	# with a comma
-print( your code here )	# with a +	-- this one should give us an error!
+print("Hello", name)	# with a comma
+# print("Hello " + name)	# with a +	-- this one should give us an error! can't add a string and a number
+print("Hello " + str(name))	# with a +	-- this one should give us an error! can't add a string and a number
+
+print("-" * 25)
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( your code here ) # with .format()
-print( your code here ) # with an f string
+print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
+
+print("-" * 25)
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
 
