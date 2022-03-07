44536b5a7600611eb66977d699b069ce68754489
Michele H <michele_h@outlook.com>
2022-03-07 15:32:08 -0800

    changes

---
+++ 3-7-2022-afternoonLecture.py
@@ -0,0 +1,24 @@
+# # variable declaration
+# a = 15 #variable a initialized as integer 15
+# def my_func(num):
+#     print("Hi from the print statement")
+#     print(num)
+#     output = a * 2 - 3
+#     return output
+
+# print(my_func(a))
+
+# print("-" * 25)
+
+a = 15
+b = 19
+c = 25
+def my_func(num):
+    print("Hi from the print statement")
+    print(num)
+    output = a * 2 - 3
+    return output
+
+my_num = my_func(a)
+
+print(my_num)
