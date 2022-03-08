0a4aa3d8e7c23e9b469b3ae04cf047a1625cf795
Michele H <michele_h@outlook.com>
2022-03-07 16:23:54 -0800

    changes

--- Fundamentals/Fundamentals/for_loop_basic1.py
+++ Fundamentals/Fundamentals/for_loop_basic1.py
@@ -16,27 +16,27 @@
 # Print integers 1 to 100. 
 # - If divisible by 5, print "Coding" instead. 
 # - If divisible by 10, print "Coding Dojo".
-num = 0
-for i in range(1, 101):
-    if num % 5 == 0:
-        print("Coding")
-    elif num % 10 == 0:
-        print("Dojo")
-    else: 
-        print(i)
+# for i in range(1, 101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else: 
+#         print(i)
 
-print("-" * 25)
+# print("-" * 25)
 
 # 4. Whoa. That Sucker's Huge
 # Add odd integers from 0 to 500,000, and print the final sum.
 
 # print("-" * 25)
-# for i in range(0, 500001 ):
-#     if num % 5:
-#         print("Coding")
-#     if num % 10:
-#         print("Dojo")
-#     print(i)
+
+# sum = 0
+
+# for i in range(0, 500001):
+#     if i % 2 == 1: #means it's an odd number, since it has a remainder of 1.
+#         sum += i
+# print(sum)
 
 # 5. Countdown by Fours
 # Print positive numbers starting at 2018, counting down by fours.
@@ -56,5 +56,9 @@ print("-" * 25)
 # lowNum = 2
 # highNum = 9
 # mult = 3
+
+# for i in range(lowNum, highNum + 1): #range(2, 10) or 2-10
+#     if i % mult == 0: #if i is divisible by 3
+#         print(i)
 
 # print("-" * 25)
