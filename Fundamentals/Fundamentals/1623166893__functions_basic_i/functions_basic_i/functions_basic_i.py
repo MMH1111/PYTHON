78d7aff1b81161986c4e14aeb15d0b27fa46e198
Michele H <michele_h@outlook.com>
2022-03-07 17:15:29 -0800

    changes

--- Fundamentals/Fundamentals/1623166893__functions_basic_i/functions_basic_i/functions_basic_i.py
+++ Fundamentals/Fundamentals/1623166893__functions_basic_i/functions_basic_i/functions_basic_i.py
@@ -1,132 +1,133 @@
-#1
-def number_of_food_groups():
-    return 5
-print(number_of_food_groups())
-
+# #1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+# #Prediction: will print 5
 
 #2
-def number_of_military_branches():
-    return 5
-print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#Prediction: will return error because number_of_days_in_a_week_silicon_or_triangle_sides() is not defined
 
+# #3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+#Prediction: will print 5 because the first return statement will execute out of the function
 
-#3
-def number_of_books_on_hold():
-    return 5
-    return 10
-print(number_of_books_on_hold())
+# #4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+#Prediction:  will print 5 because the first return statement will execute out of the function
 
+# #5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+#Prediction: will print nothing because no argument was passed into the function number_of_great_lakes()
 
-#4
-def number_of_fingers():
-    return 5
-    print(10)
-print(number_of_fingers())
+# #6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+#Prediction: will print 8 (3 + 5) // Printed 3 then 5 on separate lines
 
+# #7
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+#Prediction: will print 25
 
-#5
-def number_of_great_lakes():
-    print(5)
-x = number_of_great_lakes()
-print(x)
+# #8
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+#Prediction: will print 100, 10
 
+# #9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#Prediction: will print 7, 15 // printed out 7, 14, 21
 
-#6
-def add(b,c):
-    print(b+c)
-print(add(1,2) + add(2,3))
+# #10
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+#Prediction: will print 8
 
+# #11
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+#Prediction: will print 500, 500, 300, 500
 
-#7
-def concatenate(b,c):
-    return str(b)+str(c)
-print(concatenate(2,5))
+# #12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+#Prediction: will print 500 500 300 500
 
+# #13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+#Prediction: will print 500 500 300 300
 
-#8
-def number_of_oceans_or_fingers_or_continents():
-    b = 100
-    print(b)
-    if b < 10:
-        return 5
-    else:
-        return 10
-    return 7
-print(number_of_oceans_or_fingers_or_continents())
+# #14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+#Prediction: will print 1 3 2
 
-
-#9
-def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-    if b<c:
-        return 7
-    else:
-        return 14
-    return 3
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
-
-#10
-def addition(b,c):
-    return b+c
-    return 10
-print(addition(3,5))
-
-
-#11
-b = 500
-print(b)
-def foobar():
-    b = 300
-    print(b)
-print(b)
-foobar()
-print(b)
-
-
-#12
-b = 500
-print(b)
-def foobar():
-    b = 300
-    print(b)
-    return b
-print(b)
-foobar()
-print(b)
-
-
-#13
-b = 500
-print(b)
-def foobar():
-    b = 300
-    print(b)
-    return b
-print(b)
-b=foobar()
-print(b)
-
-
-#14
-def foo():
-    print(1)
-    bar()
-    print(2)
-def bar():
-    print(3)
-foo()
-
-
-#15
-def foo():
-    print(1)
-    x = bar()
-    print(x)
-    return 10
-def bar():
-    print(3)
-    return 5
-y = foo()
-print(y)
+# #15
+# def foo():
+#     print(1)
+#     x = bar()
+#     print(x)
+#     return 10
+# def bar():
+#     print(3)
+#     return 5
+# y = foo()
+# print(y)
+#Prediction: will print 1 3 5 10
